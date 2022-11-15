8b9daa1f52ce4727e82858aefa29534e568a8862
nefelibatala1993 <leuela1993@gmail.com>
2022-11-15 17:27:40 +0300

    Added 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,36 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_s - measures the height of a binary tree
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_height_balanced(tree->left) -
+			binary_tree_height_balanced(tree->right));
+}
+
+/**
+ * binary_tree_height_balanced - measures the height of a binary tree
  * @tree: is a pointer to the root node of the tree to measure
  * the height
  *
  * Return: On success, it returns the height of the binary tree.
  * On error, it returns 0.
 */
-size_t binary_tree_height_s(const binary_tree_t *tree)
+size_t binary_tree_height_balanced(const binary_tree_t *tree)
 {
-	size_t leftH, rightH;
-
+	size_t leftH = 0, rightH = 0;
+	
 	if (tree == NULL)
 		return (0);
-
+	
 	leftH = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-	rightH = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-	return ((leftH > rightH) ? leftH : rightH);
-}
-
-/**
- * binary_tree_balance - measures the balance factor of binary tree
- * @tree: is a pointer to the root node of the tree to measure
- * the balance factor
- *
- * Return: On success, it returns the balance factor of the binary
- * tree. Otherwise, it returns 0.
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (binary_tree_height_s(tree->left) - binary_tree_height_s(tree->right));
+	rightH = tree->right ? 1 + binary_tree_height(tree->right): 1;
+	return((leftH > rightH) ? leftH : rightH);
 }
--- Makefile
+++ Makefile
@@ -6,7 +6,8 @@ CC = gcc
 CFLAGS = -Wall -Werror -Wextra -pedantic -std=gnu89
 SRC = 0-binary_tree_node.c binary_tree_print.c 1-binary_tree_insert_left.c 2-binary_tree_insert_right.c \
 	  3-binary_tree_delete.c 4-binary_tree_is_leaf.c 5-binary_tree_is_root.c 6-binary_tree_preorder.c \
-	  7-binary_tree_inorder.c 8-binary_tree_postorder.c 9-binary_tree_height.c main.c
+	  7-binary_tree_inorder.c 8-binary_tree_postorder.c 9-binary_tree_height.c 10-binary_tree_depth.c \
+	  11-binary_tree_size.c 12-binary_tree_leaves.c 13-binary_tree_nodes.c 14-binary_tree_balance.c main.c
 OBJ = $(SRC:.c=.o)
 NAME = a
 RM = rm
