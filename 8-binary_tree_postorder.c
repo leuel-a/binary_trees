66ff83a74f50adfabedfaa429d0e291b95e85314
nefelibatala1993 <leuela1993@gmail.com>
2022-11-15 07:10:54 +0300

    Added 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,15 +5,15 @@
  * post-order traversal
  * @tree: is a pointer to the root node of the tree to traverse
  * @func: is a pointer to a function to call for each node.
- * 
+ *
  * Return: Always nothing.
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree != NULL && func != NULL)
-    {
-        binary_tree_postorder(tree->left, func);
-        binary_tree_postorder(tree->right, func);
-        (*func)(tree->n);
-    }
+	if (tree != NULL && func != NULL)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		(*func)(tree->n);
+	}
 }
