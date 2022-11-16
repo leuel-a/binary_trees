485f1a452b411c8956f13f5662ad228c1cd87e10
nefelibatala1993 <leuela1993@gmail.com>
2022-11-16 11:18:18 +0300

    Added 16-binary_tree_is_perfect.c

--- binary_trees.h
+++ binary_trees.h
@@ -44,6 +44,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 size_t binary_tree_height_balanced(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+int tree_is_perfect(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
 
 #endif
