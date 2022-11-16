f4e175099b9079f69ae4368b80fda89e7238cb67
nefelibatala1993 <leuela1993@gmail.com>
2022-11-16 15:18:26 +0300

    Added 17-binary_tree_sibling.c

--- binary_trees.h
+++ binary_trees.h
@@ -47,5 +47,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int find_depth_left(const binary_tree_t *tree);
 int isPerfect(const binary_tree_t *tree, int depth, int level);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
 
 #endif
