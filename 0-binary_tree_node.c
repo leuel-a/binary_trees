2c50bd61314f6f8a5c17944defffb7870ef6ae4d
nefelibatala1993 <leuela1993@gmail.com>
2023-02-02 08:28:08 +0300

    pass betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,14 +10,13 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
+	binary_tree_t *new;
 
-    new = malloc(sizeof(binary_tree_t));
-    if (!new)
-        return (NULL);
-
-    new->parent = parent;
-    new->n = value;
-    new->left = NULL, new->right = NULL;
-    return (new);
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL, new->right = NULL;
+	return (new);
 }
